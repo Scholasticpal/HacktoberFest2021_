a91633901ab6cb1d939923eb8f5c1d2a0eb3f659
Apurva Kumar Gupta <akgupta24092002@gmail.com>
2021-10-21 13:24:13 +0530

    Quick Sort algo added

---
+++ QuickSort_Apurva.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+void swap(int arr[],int i,int j)
+{
+    int temp=arr[i];
+    arr[i]=arr[j];
+    arr[j]=temp;
+}
+int partition(int arr[],int l,int r)
+{
+    int pivot=arr[r];
+    int i=l-1,j;
+    for(j=l;j<r;j++)
+    {
+        if(arr[j]<pivot)
+        {
+            i++;
+            swap(arr,i,j);
+        }
+    }    
+    swap(arr,i+1,r);
+    return i+1;
+}
+void quickSort(int arr[],int l,int r)
+{
+    
+    if(l<r)
+    {
+        int pi=partition(arr,l,r);
+        quickSort(arr,l,pi-1);
+        quickSort(arr,pi+1,r);
+    }
+}
+int main()
+{
+    int n;
+    cin>>n;
+    int i,arr[n];
+    for(i=0;i<n;i++)
+    cin>>arr[i];
+    quickSort(arr,0,n-1);
+    for(i=0;i<n;i++)
+    cout<<arr[i]<<" ";
+}
