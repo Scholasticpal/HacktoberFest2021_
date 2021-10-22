8c93104ba487cd94444ca21738d0862c3cc9ee63
kantesreenithin <71339114+kantesreenithin@users.noreply.github.com>
2021-10-22 17:30:27 +0530

    adding calculator using cpp

--- C++/Calculator/calculator.cpp
+++ C++/Calculator/calculator.cpp
@@ -1,38 +1,34 @@
-# include <iostream>
+#include<iostream>
 using namespace std;
-
-int main() {
-    char op;
-    float num1, num2;
-
-    cout << "Enter operator: +, -, *, /: ";
-    cin >> op;
-
-    cout << "Enter two operands: ";
-    cin >> num1 >> num2;
-
-    switch(op) {
-        case '+':
-            cout << num1 << " + " << num2 << " = " << num1 + num2;
-            break;
-
-        case '-':
-            cout << num1 << " - " << num2 << " = " << num1 - num2;
-            break;
-
-        case '*':
-            cout << num1 << " * " << num2 << " = " << num1 * num2;
-            break;
-
-        case '/':
-            cout << num1 << " / " << num2 << " = " << num1 / num2;
-            break;
-
-        default:
-            // If the operator is other than +, -, * or /, error message is shown
-            cout << "Error! operator is not correct";
-            break;
-    }
-
-    return 0;
+int main()
+{
+	char op;
+	cout<<"Enter opertor (+,-,*,/):"<<endl;
+	cin>>op;
+	double num1,num2;
+	cout<<"Enter first number:";
+	cin>>num1;
+	cout<<"Enter second number:";
+	cin>>num2;
+	switch(op)
+		{
+			case'+':
+				cout<<num1<<"+"<<num2<<"="<<(num1+num2);
+				break;
+			case'-':
+				cout<<num1<<"-"<<num2<<"="<<(num1-num2);
+				break;
+			case'*':
+				cout<<num1<<"*"<<num2<<"="<<(num1*num2);
+				break;
+			case'/':
+				if(num2!=0.0)
+				   cout<<num1<<"/"<<num2<<"="<<(num1/num2);
+				else
+				   cout<<"divide by zero stiuation";
+				break;
+	        default:
+	        	cout<<"Invalid operator";
+		}
+		return 0;
 }
