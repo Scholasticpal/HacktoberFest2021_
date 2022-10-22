6118bae250ab2bf7f9a8828a8e2d1d1d0523eae7
Siwar Ayed <76560555+siwar9@users.noreply.github.com>
2022-10-22 13:22:02 +0100

    My first participation in Hacktoberfest! Given a sequence a1,a2,…,an, find the minimum number of elements to remove from the sequence such that after the removal, the sum of every 2 consecutive elements is even.

---
+++ C++/BruteForceGreedyMath.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main()
+{
+    int t;
+    cin>>t;
+    while (t--){
+        int n;
+        cin>>n;
+        int od = 0, ev = 0;
+ 
+        for (int i = 0 ; i<n ; i++){
+            int x;
+            cin>>x;
+            if (x & 1){
+                od++;
+            }
+            else {ev++;}
+        }
+        cout<<min(od,ev)<<endl;
+        }
+ 
+}
